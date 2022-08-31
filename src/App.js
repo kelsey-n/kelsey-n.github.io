2181d2f3ce9dbfedc8349c364c92260a525e94da
kelsey-n <kan317@nyu.edu>
2022-08-31 10:52:41 -0400

    imported chakra, styled button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
 import "./App.css";
 import Bulbs from "./Mood";
 
 function App() {
   return (
-    <div className="App">
-      <Bulbs />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Bulbs />
+      </div>
+    </ChakraProvider>
   );
 }
 
--- src/Mood.js
+++ src/Mood.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import { Button } from "@chakra-ui/react";
 
 import "./App.css";
 
-function Bulbs() {
-  const [on, setOn] = useState(false);
+const buttonColor = "pink";
 
-  const lightSwitch = () => setOn((on) => !on);
+function Heart() {
+  const [mood, setMood] = useState(false);
+
+  const moodSwitch = () => setMood((mood) => !mood);
 
   return (
     <>
-      <div className={on ? "bulb-on" : "bulb-off"} />
-      <button onClick={lightSwitch}>Happy/Sad</button>
+      <div className={mood ? "mood-happy" : "mood-sad"} />
+      <Button onClick={moodSwitch} colorScheme={buttonColor} size={"sm"}>
+        Toggle Mood
+      </Button>
     </>
   );
 }
 
-export default Bulbs;
+export default Heart;
